3677b1f3fb62adb04104642fedf95512b5f6b743
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2021-11-27 15:42:10 +0200

    dz 91

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyColor = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
 
+const changeColor = 1000;
+let intervalId = null;
+
+startBtn.addEventListener('click', onStartBtn);
+stoptBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  intervalId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, changeColor);
+  startBtn.disabled = true;
+}
+
+function onStopBtn() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
